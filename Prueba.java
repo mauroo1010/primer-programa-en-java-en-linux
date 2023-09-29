ee37a83c2e36eb00f4eb4b17010518e630a12974
Mauro <maurogutierreztormo@gmail.com>
2023-09-29 13:34:21 +0200

    Segundo commit

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,7 @@ public class Prueba{
 	public static void main(String args[]){
 	
 		System.out.println("Mi deporte favorito es el futbol");
-		
+		System.out.println("Mi lugar favorito es Cangas");
 		
 	}
 	
